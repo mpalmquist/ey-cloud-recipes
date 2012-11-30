fac147d34881345aefa29d345b9da3e03d0a4338
ilozka <ilozka@gmail.com>
2012-11-30 11:05:26 +0200

    dont require passenger recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -5,7 +5,7 @@
 #end
 
 # uncomment to turn on thinking sphinx/ultra sphinx. Remember to edit cookbooks/sphinx/recipes/default.rb first!
-require_recipe "passenger_monitor_memory_limit"
+# require_recipe "passenger_monitor_memory_limit"
 
 # require_recipe "sphinx"
 
@@ -112,7 +112,7 @@ require_recipe "passenger_monitor_memory_limit"
   # postgresql9_isn "dbname"
   # postgresql9_lo "dbname"
   # postgresql9_ltree "dbname"
-  # postgresql9_pg_trgm "dbname"  
+  # postgresql9_pg_trgm "dbname"
   # postgresql9_pgcrypto "dbname"
   # postgresql9_pgrowlocks "dbname"
   # postgresql9_postgis "dbname"
@@ -122,15 +122,15 @@ require_recipe "passenger_monitor_memory_limit"
   # postgresql9_test_parser "dbname"
   # postgresql9_unaccent "dbname"
   # postgresql9_uuid_ossp "dbname"
-  
-  
+
+
   # 9.1 Extensions
-  # postgresql9_file_fdw "dbname" 
+  # postgresql9_file_fdw "dbname"
   # postgresql9_xml2 "dbname"
-  
+
   #Admin-Level Contribs
   # postgresql9_pg_buffercache "postgres"
   # postgresql9_pg_freespacemap "postgres"
   # postgresql9_pg_stat_statements "todo" - Not done
-  
+
 # end
