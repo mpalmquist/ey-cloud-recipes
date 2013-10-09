c39e68872d690d41971d5002210f9412f3d0f5ff
Zoltan Csoma <ilozka@gmail.com>
2013-10-09 21:04:10 +0300

    use sphinx 2.0.8

--- cookbooks/emerge/recipes/default.rb
+++ cookbooks/emerge/recipes/default.rb
@@ -13,12 +13,12 @@
 # $ eix libxml2
 #############################################
 
-enable_package "sphinx" do
-  version "0.9.9"
+enable_package "app-misc/sphinx" do
+  version "2.0.8"
 end
 
 # Install the newly unmasked version
 package "app-misc/sphinx" do
-  version "0.9.9"
+  version "2.0.8"
   action :install
 end
