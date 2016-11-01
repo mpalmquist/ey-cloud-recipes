4ae24aa5bb6051cd0e9fee0c454d9964cc473880
ilozka <ilozka@gmail.com>
2016-11-01 22:49:52 +0200

    more thinking-sphinx recipe updates

--- cookbooks/thinking-sphinx-3/recipes/default.rb
+++ cookbooks/thinking-sphinx-3/recipes/default.rb
@@ -5,8 +5,10 @@
 if solo? || util?
   Chef::Log.info "Running thinking-sphinx-3 recipe"
   include_recipe "thinking-sphinx-3::install"
-  include_recipe "thinking-sphinx-3::thinking_sphinx"
   include_recipe "thinking-sphinx-3::setup"
 else
   include_recipe "thinking-sphinx-3::cleanup"
+end
+unless db_server?
+  include_recipe "thinking-sphinx-3::thinking_sphinx"
 end
