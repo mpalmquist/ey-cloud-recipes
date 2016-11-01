01ed87dea7fbaa0c2210649f9fb244af07465c6a
ilozka <ilozka@gmail.com>
2016-11-01 10:52:47 +0200

    don't run ts on app-master

--- cookbooks/thinking-sphinx-3/recipes/default.rb
+++ cookbooks/thinking-sphinx-3/recipes/default.rb
@@ -2,13 +2,11 @@
 # Cookbook Name:: sphinx
 # Recipe:: default
 #
-
-# if solo? || !db_server?
-Chef::Log.info "run sphinx? #{solo?} || #{util?}"
 if solo? || util?
   Chef::Log.info "Running thinking-sphinx-3 recipe"
-  include_recipe "thinking-sphinx-3::cleanup"
   include_recipe "thinking-sphinx-3::install"
   include_recipe "thinking-sphinx-3::thinking_sphinx"
   include_recipe "thinking-sphinx-3::setup"
+else
+  include_recipe "thinking-sphinx-3::cleanup"
 end
