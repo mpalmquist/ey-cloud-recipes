4754b5248cf0cfd99c6ba007b7d245996aa3a8ec
ilozka <ilozka@gmail.com>
2017-01-25 13:47:35 +0200

    add logging

--- cookbooks/memcached/recipes/default.rb
+++ cookbooks/memcached/recipes/default.rb
@@ -12,6 +12,7 @@ end
 node[:applications].each do |app_name,data|
   user = node[:users].first
   if %w(solo app app_master util).include? node[:instance_role]
+    Chef::Log.info "Creating memcached.yml, instance role #{node[:instance_role]}"
     template("/data/#{app_name}/shared/config/memcached_custom.yml") do
       source "memcached.yml.erb"
       owner user[:username]
@@ -22,6 +23,7 @@ node[:applications].each do |app_name,data|
   end
 
   if %w(solo util).include? node[:instance_role]
+    Chef::Log.info "Creating memcached config, instance role #{node[:instance_role]}"
     template "/etc/conf.d/memcached" do
       owner 'root'
       group 'root'
@@ -30,10 +32,15 @@ node[:applications].each do |app_name,data|
       variables :memusage => 64, :port => 11211
     end
   else
+    Chef::Log.info "Clean up memcached, instance role #{node[:instance_role]}"
     file "/etc/monit.d/memcached.monitrc" do
       action :delete
       notifies :run, resources(:execute => "reload-monit")
       only_if "test -f /etc/monit.d/memcached.monitrc"
+    end
+    file "/etc/conf.d/memcached" do
+      action :delete
+      only_if "test -f /etc/conf.d/memcached"
     end
     execute "kill-memcached" do
       command "pkill -f memcached"
